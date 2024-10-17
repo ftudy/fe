11ef5364fe4a1a002af6674fdb602092c2addc28
Juyeon Kim <kkjuyeon@gmail.com>
2024-10-17 20:34:38 +0900

    chore: eslint 규칙 추가
    - 미사용 변수 패턴 정의
    - import order 그룹 정의
    - 저장 시 자동 수정 설정 추가

--- README.md
+++ README.md
@@ -6,3 +6,27 @@ pnpm 및 turbo의 전역 설치가 필요합니다.
 npm install -g pnpm
 pnpm install turbo --global
 ```
+
+모든 프로젝트 설치 패키지 설치
+
+```
+pnpm install
+```
+
+모노레포 실행
+```
+pnpm run dev
+```
+
+### 예상 오류
+Q1. eslint 설정 미작동 시
+
+A1. eslint 캐시 삭제 및 eslint 서버 재시작
+
+Step1. (mac 기준) 대상 프로젝트 폴더로 이동 후 eslint cache를 비워줍니다.
+
+```
+rm -f .eslintcache
+```
+Step2.  eslint 서버 재시작. command + shift + p > Restart ESLint server 클릭합니다. 
+
--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -18,7 +18,6 @@ module.exports = {
     node: true,
     browser: true,
   },
-  plugins: ["only-warn"],
   settings: {
     "import/resolver": {
       typescript: {
@@ -26,10 +25,55 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
+  ignorePatterns: [".*.js", "node_modules/"],
   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: false,
+        varsIgnorePattern: "^_", // 미사용 변수로 취급하지 않을 변수명 패턴
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        // groups 배열 순서대로 그룹 순서 지정
+        groups: [
+          ["builtin", "external"], // node.js 기본 모듈과 npm으로 설치한 외부 모듈 그룹
+          "internal", // 프로젝트 내 생성한 모듈
+          ["parent", "sibling"], // 부모 디렉토리(../)에 있는 모듈과 같은 디렉토리(./) 레벨에 있는 모듈
+          "index", // 현재 디렉토리의 index.js | .ts 파일
+          "object", // 모듈 전체를 객체로 가져올 때 (e.g. import * as utils from "./")
+        ],
+        pathGroups: [
+          // group으로 지정할 패턴 정의
+          {
+            pattern: "~/**",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@*",
+            group: "external",
+            position: "after",
+          },
+          {
+            pattern: "@*/**",
+            group: "external",
+            position: "after",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react", "react-dom"], // 순서 정렬에서 제외 후 최상단 위치
+        "newlines-between": "always", // 그룹 사이 빈 줄 추가
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
